7319bb4898363de28b18b71cc5c89ff30f47ea41
msp3021 <131592979+msp3021@users.noreply.github.com>
2024-09-23 21:44:06 +0200

    changes

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,10 @@
+
 import { defineConfig } from 'vite';
 import glob from 'glob';
 import injectHTML from 'vite-plugin-html-inject';
 import FullReload from 'vite-plugin-full-reload';
+ 
+
 
 export default defineConfig(({ command }) => {
   return {
